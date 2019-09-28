ff02bcd9ce1497ba6cc2ae43a8b28ced123b237c
Jazmin M <jazminmerckel@gmail.com>
2019-09-28 01:39:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,4 +20,10 @@ return rooms
 end
 
 def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+  puts badge
+  end
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
+  end
 end
