e921a9af4ff6842ce40dae3399b558512d3a13d5
Jazmin M <jazminmerckel@gmail.com>
2019-09-28 01:28:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(names)
+  return "Hello, my name is #{names}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendee|
+    badges << "Hello, my name is #{attendee}."
+  end
+return badges
+end
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each do |attendee|
+    rooms << "Hello, #{attendee}! You'll be assigned to room #{attendee.count}."
+  end
+return rooms
+end
+
+def printer(attendees)
+end
